547d16805668fbdbd1ba9c931e469fe3b8684791
ramsahnp@gmail.com <ramsahnp@gmail.com>
2018-05-16 16:32:36 +0530

    hs3jdeg

---
+++ collections/src/collections/ListTest.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+
+		ArrayList l=new ArrayList();
+		//"hvj);
+	}
+
+}
